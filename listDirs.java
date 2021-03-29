595756e128e67a13ed926190bb19486e2de6f4a1
Carl <carl.last@gmail.com>
2021-03-29 11:21:06 +0100

    udpate tested

--- listDirs.java
+++ listDirs.java
@@ -1,16 +1,66 @@
 import java.io.*;
 import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.io.IOException;
+import java.util.Scanner;
 public class listDirs {
-
-    public static void main(String[] args) {
-	    File f = new File(".");
-	    File[] files = f.listFiles();
-	    Arrays.sort(files);
-            // Get the names of the files by using the .getName() method
-            for (int i = 0; i < files.length; i++) {
-                if ( files[i].isDirectory()) {
-		  System.out.println(files[i].getName());
-		}
-            }
-        }
+    public static void main(String[] args) throws IOException, InterruptedException {
+      if (args.length != 2) {
+        print("Usage: java listDirs source_dir target_dir");
+        System.exit(1);
+      }
+      loadVersions(args[0], args[1]);
     }
+    
+    public static void loadVersions(String path, String targetpath) throws IOException, InterruptedException {
+      int foundversions = 0;
+      File f = new File(path);
+      File[] files = f.listFiles();
+      Arrays.sort(files);
+      for (int i = 0; i < files.length; i++) {
+        if ( files[i].isDirectory() && files[i].getName().substring(0,2).equals("1.") ) {
+          // Load version
+          Path source = Paths.get(files[i].getPath()+"/"+files[i].getParentFile().getName());
+          String targetstring = targetpath+files[i].getParentFile().getPath().substring(1)+"/"+files[i].getParentFile().getName();
+          Path target = Paths.get(targetstring);
+          print(files[i].getParentFile().getName()+":v"+files[i].getName());
+          Files.copy(source,target,StandardCopyOption.REPLACE_EXISTING );
+          // Get commit message
+          String commitfile = files[i].getPath()+"/commit.txt";
+          // commit current version and cross fingers
+          String[] addcommands = {"svn", "add", targetstring};
+          Process addprocess = Runtime.getRuntime().exec(addcommands);
+          int addexitcode = addprocess.waitFor();
+          monitorProcess(addprocess);
+          String[] cicommands = {"svn", "ci", targetstring, "-F", commitfile};
+          Process ciprocess = Runtime.getRuntime().exec(cicommands);
+          int ciexitcode = ciprocess.waitFor();
+          monitorProcess(ciprocess);
+          //Process ciprocess = Runtime.getRuntime().exec("bash -c \"svn ci "+targetstring+" -F "+commitfile+"\"");
+        } else {
+          if (files[i].isDirectory()) loadVersions(files[i].getPath(),targetpath);
+        }
+      }
+    }
+    
+    public static void monitorProcess(Process proc) throws IOException {
+          BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));  
+          BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream())); 
+          // Read the output from the command 
+          String s = null; 
+          while ((s = stdInput.readLine()) != null) {     
+            print(s); 
+          }  
+          // Read any errors from the attempted command 
+          while ((s = stdError.readLine()) != null) {     
+            System.out.println(s); 
+          }
+    }
+    
+    public static void print(String msg) {
+      System.out.println(msg);
+    }
+}
